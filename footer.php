73590a26e07d191a4b9c72130be7b4ff1d659846
Ayo Ayco <ramon.aycojr@gmail.com>
2014-07-30 03:05:03 +0800

    optimized scripts 2

--- footer.php
+++ footer.php
@@ -8,6 +8,8 @@
           </p>
       </div>
      </div>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
     <script>
 		$(function () {
 		  $('[data-toggle="tooltip"]').tooltip();
