0cc11a237ad2acb241ed41c0ac96e1d4b1eaaf53
Ayo Ayco <ramon.aycojr@gmail.com>
2014-07-30 00:45:18 +0800

    new features: add/delete timeline posts

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <div id="footer">
       <div class="container">
           <p class="muted credit text-center" style="font-size: x-small;">
-            <?php if(date("Y")!="2014") echo "2014-"; echo date("Y");?> &copy; Ayo Ayco. All Rights Reserved. <span id="mobile-first-info" data-toggle="tooltip" data-placement="top" title='This website is designed to look good on any screen. Enjoy. :)'>Mobile First Design.</span>
+            <?php if(date("Y")!="2014") echo "2014-"; echo date("Y");?> &copy; Ayo Ayco. All Rights Reserved. <span id="mobile-first-info" data-toggle="tooltip" data-placement="top" title='This website is designed to look good on any screen. Enjoy. :)'>Mobile First Design by Ayo.</span>
           </p>
       </div>
      </div>
