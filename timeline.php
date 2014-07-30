bf423cd47e7865ece8d8b324e4f1ed410820cce0
Ayo Ayco <mail@ayoayco.com>
2014-07-31 04:49:33 +0800

    fixed animation in timeline

--- timeline.php
+++ timeline.php
@@ -40,29 +40,7 @@
           </div>
 
       <?php } ?>
-    
-    <script>
-      (function() {
-        $(document).ready(function() {
-          var timelineAnimate;
-          timelineAnimate = function(elem) {
-            return $(".timeline.animated .timeline-row").each(function(i) {
-              var bottom_of_object, bottom_of_window;
-              bottom_of_object = $(this).position().top + $(this).outerHeight();
-              bottom_of_window = $(window).scrollTop() + $(window).height();
-              if (bottom_of_window > bottom_of_object) {
-                return $(this).addClass("active");
-              }
-            });
-          };
-          timelineAnimate();
-          return $(window).scroll(function() {
-            return timelineAnimate();
-          });
-        });
-
-      }).call(this);
-    </script>
 </div>
     <div style="height: 30px;"></div>
 </div>
+    <script src="js/timeline.js"></script>
