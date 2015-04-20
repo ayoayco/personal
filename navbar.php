c081ec23b63a22dc854ca12577c0e5e9da90f8f9
Ayo Ayco <ramon.aycojr@gmail.com>
2015-04-20 20:16:48 +0800

    updated to 2.0

--- navbar.php
+++ navbar.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="?id=home">AYO AYCO</a>
+      <a class="navbar-brand" href="?id=home">AYO AYCO 2.0</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
