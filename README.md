755dbde614e3efc587b5f0ebeb4daf09071a4906
Ayo <ramon.aycojr@gmail.com>
2016-02-16 16:58:20 +0800

    dbconnect_template and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the source code for my personal website: http://ayoayco.com
+
+please rename the file "dbconnect_template.php" to "dbconnect.php" upon setup and change the variables within as you see fit.
---
+++ dbconnect_template.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+	$host="hostname"; // Host name
+	$username="username"; // Mysql username
+	$password="password!"; // Mysql password
+	$db_name="database name"; // Database name
+
+	// Connect to server and select databse.
+	$con = mysql_connect("$host", "$username", "$password")or die("cannot connect");
+	mysql_select_db("$db_name")or die("cannot select DB");
+
+?>
+
