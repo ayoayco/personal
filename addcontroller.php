224e85d3fea5a81fbe3b4b491a2d00ddc4921fc1
Ayo <mail@ayoayco.com>
2016-12-23 16:16:30 +0800

    use mysql_real_escape_string() to sanitize queries

--- addcontroller.php
+++ addcontroller.php
@@ -14,9 +14,11 @@ if($monthday == ""){
 
 @include('dbconnect.php');
 $sql="INSERT INTO `timeline_post`(`month-day`, `year`, `icon`, `context`, `title`, `body`) VALUES ('".$monthday."', '".$year."', '".$icon."', '".$context."', '".$title."', '".$body."')";
+$sql = mysql_real_escape_string($sql);
 $result = mysql_query($sql) or die ("Cannot add Life Moment!");
 
 $sql2 = "SELECT * FROM `options` WHERE `option` = 'url'";
+$sql2 = mysql_real_escape_string($sql2);
 $result2 = mysql_query($sql2) or die ("Cannot retrieve url!");
 $row = mysql_fetch_array($result2);
 
