769a2aaad528064adfa277fb79479dec33f0035c
Ayo <mail@ayoayco.com>
2016-12-23 16:43:29 +0800

    undebug edit

--- editcontroller.php
+++ editcontroller.php
@@ -13,18 +13,15 @@ if($monthday == ""){
     $monthday = "Sometime in";
 }
 
-//echo $projID;
-
 @include('dbconnect.php');
 $sql="UPDATE `timeline_post` SET `month-day` = '".$monthday."', `year` = '".$year."', `icon`= '".$icon."', `context` = '".$context."', `title` = '".mysql_real_escape_string($title)."', `body` = '".mysql_real_escape_string($body)."' WHERE `id` = '".$projID."'";
-echo $sql."<br />";
 
-/*$result = mysql_query($sql) or die ("Cannot edit Life Moment!");
+$result = mysql_query($sql) or die ("Cannot edit Life Moment!");
 
 $sql2 = "SELECT * FROM `options` WHERE `option` = 'url'";
 $result2 = mysql_query($sql2) or die ("Cannot retrieve url!");
 $row = mysql_fetch_array($result2);
 
 header("Location: ".$row['value']."?id=home");
-*/
+
 ?>
