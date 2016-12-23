28f8b6fdd101bd03ab60bc93d002725c790cd92c
Ayo <ramon.aycojr@gmail.com>
2016-12-23 16:29:10 +0800

    sanitize values for add and edit controllers

--- addcontroller.php
+++ addcontroller.php
@@ -1,12 +1,12 @@
 
 <?php
 
-$monthday = $_POST['month-day'];
-$year = $_POST['year'];
-$icon = $_POST['icon'];
-$context = $_POST['context'];
-$title = $_POST['title'];
-$body = $_POST['body'];
+$monthday = mysql_real_escape_string($_POST['month-day']);
+$year = mysql_real_escape_string($_POST['year']);
+$icon = mysql_real_escape_string($_POST['icon']);
+$context = mysql_real_escape_string($_POST['context']);
+$title = mysql_real_escape_string($_POST['title']);
+$body = mysql_real_escape_string($_POST['body']);
 
 if($monthday == ""){
     $monthday = "Sometime in";
@@ -14,11 +14,9 @@ if($monthday == ""){
 
 @include('dbconnect.php');
 $sql="INSERT INTO `timeline_post`(`month-day`, `year`, `icon`, `context`, `title`, `body`) VALUES ('".$monthday."', '".$year."', '".$icon."', '".$context."', '".$title."', '".$body."')";
-$sql = mysql_real_escape_string($sql);
 $result = mysql_query($sql) or die ("Cannot add Life Moment!");
 
 $sql2 = "SELECT * FROM `options` WHERE `option` = 'url'";
-$sql2 = mysql_real_escape_string($sql2);
 $result2 = mysql_query($sql2) or die ("Cannot retrieve url!");
 $row = mysql_fetch_array($result2);
 
--- checklogin.php
+++ checklogin.php
@@ -6,7 +6,6 @@
 	$password = md5($_POST['password']);
 
 	$sql = "SELECT password FROM user";
-	$sql = mysql_real_escape_string($sql);
 	$result = mysql_query($sql);
 	$row = mysql_fetch_array($result);
 
@@ -16,7 +15,6 @@
 		header('Location: ?id=timeline');
 	}else{
 		$sql = "SELECT value FROM `options` WHERE `option` LIKE 'url'";
-		$sql = mysql_real_escape_string($sql);
 		$result = mysql_query($sql) or die("Cannot execute query!");
 		$row = mysql_fetch_array($result);
 		header('Location: '.$row['value']);
