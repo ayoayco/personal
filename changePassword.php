057880bb2ba7e867c007c1e2d352d8bb98ad3185
Ayo <mail@ayoayco.com>
2016-12-23 17:06:05 +0800

    add change password page

---
+++ changePassword.php
@@ -0,0 +1,36 @@
+<div class="container">
+		<div class="row text-center" style="margin-bottom: 20px;">
+			<h1><i class="fa fa-pencil"></i> Change your Password</h1>
+		</div>
+		<form method='post' action='addcontroller.php' class="form-horizontal" role="form">
+			<div class="row">
+				<div class="col-md-4  col-md-offset-4">
+					  <div class="form-group">
+					    <div class="input-group">
+					      <div class="input-group-addon"><i class="fa fa-arrow-right"></i></div>
+					      <input type="password" id="month-day" name="month-day" class="form-control" placeholder="Old password" maxlength="15" />
+					    </div>
+					  </div>
+
+					  <div class="form-group">
+					    <div class="input-group">
+					      <div class="input-group-addon"><i class="fa fa-arrow-right"></i></div>
+					      <input type="password" id="icon" name="icon" class="form-control" placeholder="New password" maxlength="30" />
+					    </div>
+					  </div>
+
+					  <div class="form-group">
+					    <div class="input-group">
+					      <div class="input-group-addon"><i class="fa fa-arrow-right"></i></div>
+					      <input type="password" id="icon" name="icon" class="form-control" placeholder="Confirm new password" maxlength="30" />
+					    </div>
+					  </div>
+
+					<div class="form-group" style="margin-left: 50px;">
+						      <button style="margin-bottom: 5px; width: 250px;" type="submit" class="btn btn-primary">Let's go!</button><br />
+						      <a style="width: 250px;" href="?id=home" class="btn btn-danger">Cancel</a>
+					</div>
+				</div>
+			</div>
+		</form>
+</div>
