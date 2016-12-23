a6dcdda760a5b5cc615d401e9dc7bf60b92bd731
Ayo <mail@ayoayco.com>
2016-12-23 16:03:01 +0800

    add edit feature

--- delete.php
+++ delete.php
@@ -1,14 +1,14 @@
 <?php
 
-$id = $_GET['id'];
+$projID = $_GET['projID'];
 
 @include('dbconnect.php');
-$sql = "DELETE FROM `timeline_post` WHERE `id` = ".$id;
+$sql = "DELETE FROM `timeline_post` WHERE `id` = ".$projID;
 $sql2 = "SELECT * FROM `options` WHERE `option` = 'url'";
 $result2 = mysql_query($sql2) or die ("Cannot retrieve url!");
 $row = mysql_fetch_array($result2);
 if($request = mysql_query($sql))
-	header("Location: ".$row['value']."?id=timeline");
+	header("Location: ".$row['value']."?id=home");
 else
 	die("Cannot delete timeline post! (id: ".$id.")");
 
