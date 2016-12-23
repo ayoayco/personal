a81df673731e6ca9702a53c30bc384c12d6b81c6
Ayo <mail@ayoayco.com>
2016-12-23 16:36:26 +0800

    edit debug

--- editcontroller.php
+++ editcontroller.php
@@ -20,12 +20,12 @@ if($monthday == ""){
 $sql="UPDATE `timeline_post` SET `month-day` = '".$monthday."', `year` = '".$year."', `icon`= '".$icon."', `context` = '".$context."', `title` = '".$title."', `body` = '".$body."' WHERE `id` = '".$projID."'";
 echo $sql."<br />";
 
-$result = mysql_query($sql) or die ("Cannot edit Life Moment!");
+/*$result = mysql_query($sql) or die ("Cannot edit Life Moment!");
 
 $sql2 = "SELECT * FROM `options` WHERE `option` = 'url'";
 $result2 = mysql_query($sql2) or die ("Cannot retrieve url!");
 $row = mysql_fetch_array($result2);
 
 header("Location: ".$row['value']."?id=home");
-
+*/
 ?>
