3eb7e01ab087ac68276e947c49eb0349673de493
Ayo <mail@ayoayco.com>
2017-11-07 15:38:37 +0800

    add skills list

---
+++ skills.php
@@ -0,0 +1,6 @@
+<div>
+    <h3 data-toggle="tooltip" data-placement="top" title='Events where I was resource speaker.'>Web Development</h3>
+    <p>Angular, Typescript, JavaScript, CSS, HTML, NodeJS, PHP</p>
+    <p>MySQL, PostgreSQL</p>
+    <p>Jasmine, Karma, ESLint, Webpack</p>
+</div>
