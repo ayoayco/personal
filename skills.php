73caefbb85df3d6fecc22b100a3e59221529e16c
Ayo <mail@ayoayco.com>
2017-11-07 15:45:26 +0800

    add python skill

--- skills.php
+++ skills.php
@@ -1,6 +1,6 @@
 <div>
     <h3 data-toggle="tooltip" data-placement="top" title='List of skills on web development'>Web Development</h3>
-    <p>Angular, Typescript, JavaScript, CSS, HTML, NodeJS, PHP</p>
+    <p>Angular, Typescript, JavaScript, CSS, HTML, NodeJS, PHP, Python</p>
     <p>MySQL, PostgreSQL</p>
     <p>Jasmine, Karma, ESLint, Webpack</p>
 </div>
