bbf78ba37865495ffd16dcc788cc1664f5d2f5b8
Ayo <ramon.aycojr@gmail.com>
2017-11-07 15:41:27 +0800

    separate speaking list view

---
+++ speaking.php
@@ -0,0 +1,34 @@
+        <div>
+          <h3 data-toggle="tooltip" data-placement="top" title='Events where I was resource speaker.'>Speaking Engagements</h3>
+          <img class="speaklogo" style="width:100px;" src="assets/images/ics_logo.jpg">
+          <p>
+              ICS Week 2017<br>
+              Web and GIS for Disaster Risk Reduction and Management<br>
+              Feb. 16, 2017<br>
+              ICS Lecture Hall 3, UPLB, Los Ba&ntilde;os<br>
+          </p>
+          <img class="speaklogo" style="width:100px;" src="assets/images/foss4g2015.png">
+          <p>
+              Free and Open Source<br>
+              Software for Geospatial 2015<br>
+              Web and Framework<br>
+              Sep. 17, 2015<br>
+              The K Seoul Hotel, Seoul, Korea<br>
+          </p>
+          <img class="speaklogo" style="width:100px;" src="assets/images/acrs2015.png">
+          <p>
+            Asian Conference on Remote<br>
+            Sensing 2015<br>
+            WebGIS & Digital Earth<br>
+            Oct. 22, 2015<br>
+            Crowne Plaza Manila Galleria, Oritigas<br>
+          </p>
+          <img class="speaklogo" style="width:100px;" src="assets/images/devcon2015.png">
+          <p>
+            DevCon Summit 2015<br>
+            Web Development Special Track<br>
+            Nov. 14, 2015<br>
+            SMX Convention Center,<br>
+            SM Mall of Asia<br>
+          </p>
+        </div>
